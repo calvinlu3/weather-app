603f30ffb55e900b7ff8f6a1f740b834012d575c
Calvin Lu <calvin.lu@stonybrook.edu>
2021-06-12 22:19:01 -0400

    Prevent user from searching on empty input

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
 import ForcastPage from './components/ForecastPage';
 import ResultsPage from './components/ResultsPage';
+import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
 const App = () => {
   return (
     <div className='app'>
       <Router>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/search/:search' component={ResultsPage} />
-        <Route path='/weather/location=:lat,:long' component={ForcastPage} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <>
+            <Header />
+            <Route path='/search/:search' component={ResultsPage} />
+            <Route
+              path='/weather/location=:lat,:long'
+              component={ForcastPage}
+            />
+          </>
+        </Switch>
+        <Footer></Footer>
       </Router>
-      <Footer></Footer>
     </div>
   );
 };
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,22 +1,25 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 export const SearchBox = () => {
   const [inputLocation, setInputLocation] = useState('');
+  let history = useHistory();
+
+  const handleSearch = () => {
+    if (inputLocation.length === 0) return;
+    history.push(`/search/${inputLocation}`);
+  };
 
   return (
     <div className='input-group mb-3'>
-      <input
-        type='text'
-        className='form-control form-control-lg'
-        placeholder='Enter city name or zipcode'
-        onChange={(e) => setInputLocation(e.target.value)}
-      />
-      <div className='input-group-append'>
-        <button className='btn btn-light' type='button'>
-          <Link to={`/search/${inputLocation}`}>Search</Link>
-        </button>
-      </div>
+      <form className='search-form' onSubmit={handleSearch}>
+        <input
+          type='text'
+          className='form-control form-control-lg'
+          placeholder='Enter city name or zipcode'
+          onChange={(e) => setInputLocation(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
